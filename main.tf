a6a0c5f478c2e2dc5ccd0bd59d329d3fbc8a6dbc
Nico TSESSILEDE <31391553+nicotsessilede@users.noreply.github.com>
2022-05-14 00:25:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,6 +32,63 @@ resource "azurerm_public_ip" "pip1" {
   sku                 = "Standard"
 }
 
+
+
+resource "azurerm_application_gateway" "network" {
+  name                = "myAppGateway"
+  resource_group_name = azurerm_resource_group.rg1.name
+  location            = azurerm_resource_group.rg1.location
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.frontend.id
+  }
+
+  frontend_port {
+    name = var.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = var.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.pip1.id
+  }
+
+  backend_address_pool {
+    name = var.backend_address_pool_name
+    ip_addresses = [data.azurerm_container_group.mycon.ip_address, "2.3.4.5"]
+  }
+
+  backend_http_settings {
+    name                  = var.http_setting_name
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = var.listener_name
+    frontend_ip_configuration_name = var.frontend_ip_configuration_name
+    frontend_port_name             = var.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = var.request_routing_rule_name
+    rule_type                  = "Basic"
+    http_listener_name         = var.listener_name
+    backend_address_pool_name  = var.backend_address_pool_name
+    backend_http_settings_name = var.http_setting_name
+  }
+}
+
 resource "azurerm_container_group" "Container_instances" {
   name                = "dev-continst"
   location            = azurerm_resource_group.rg1.location
@@ -60,62 +117,8 @@ resource "azurerm_container_group" "Container_instances" {
   }
 }
 
-resource "azurerm_application_gateway" "network" {
-  name                = "myAppGateway"
-  resource_group_name = azurerm_resource_group.rg1.name
-  location            = azurerm_resource_group.rg1.location
-
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 2
-  }
-
-  gateway_ip_configuration {
-    name      = "my-gateway-ip-configuration"
-    subnet_id = azurerm_subnet.frontend.id
-  }
-
-  frontend_port {
-    name = var.frontend_port_name
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                 = var.frontend_ip_configuration_name
-    public_ip_address_id = azurerm_public_ip.pip1.id
-  }
-
-  backend_http_settings {
-    name                  = var.http_setting_name
-    cookie_based_affinity = "Disabled"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 60
-  }
-
-  http_listener {
-    name                           = var.listener_name
-    frontend_ip_configuration_name = var.frontend_ip_configuration_name
-    frontend_port_name             = var.frontend_port_name
-    protocol                       = "Http"
-  }
-
-  request_routing_rule {
-    name                       = var.request_routing_rule_name
-    rule_type                  = "Basic"
-    http_listener_name         = var.listener_name
-    backend_address_pool_name  = var.backend_address_pool_name
-    backend_http_settings_name = var.http_setting_name
-  }
-
-  backend_address_pool {
-    name = var.backend_address_pool_name
-    ip_addresses = [data.azurerm_container_group.mycon.ip_address, "2.3.4.5"]
-  }
 
 data "azurerm_container_group" "mycon" {
   name                = "dev-hello-world"
-  resource_group_name = "myResourceGroupAG"
-}
+  resource_group_name = azurerm_resource_group.rg1.name
 }
